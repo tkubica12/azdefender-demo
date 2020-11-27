29861ed96fa9c1eb3f0f12d5f0cc99b1c5982726
Tomas Kubica <tkubica12@gmail.com>
2020-11-27 14:57:40 +0100

    refactor app to use managed identity to sql

--- scripts/prepareWindows.ps1
+++ scripts/prepareWindows.ps1
@@ -1,18 +1,36 @@
 param (
     [string]$sqlConnectionString,
     [string]$clientId,
-    [string]$clientSecret
+    [string]$clientSecret,
+    [string]$sqlLoginIdentity
 )
 
 # Copy install files to proper locations
 New-Item -Path "c:\" -Name "install" -ItemType "directory"
-Copy-Item MicrosoftEdgeSetupBeta.exe C:\install\
 Copy-Item azuredatastudio-windows-user-setup-1.23.0.exe C:\install\
 Copy-Item app.zip C:\install\
 Copy-Item sqlpackage-win7-x64-en-US-15.0.4897.1.zip C:\install\
 Copy-Item contosoclinic.bacpac C:\install\
 Copy-Item MsSqlCmdLnUtils.msi C:\install\
 
+# Install Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# Install Edge
+choco install microsoft-edge -y
+
+# Install Azure Data Studio
+choco install azure-data-studio -y
+
+# Install sqlpackage
+choco install sqlpackage -y
+
+# Install sqlcmd
+choco install sqlserver-odbcdriver -y
+choco install sqlserver-cmdlineutils -y
+
+# Reload PATH
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
 # Install IIS
 Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole
@@ -40,10 +58,20 @@ Install-Module -Name Az -Force
 Install-Module -Name SqlServer -Force
 
 # Import database structure and data
-cd C:\install
-Expand-Archive -LiteralPath 'C:\install\sqlpackage-win7-x64-en-US-15.0.4897.1.zip'
-C:\install\sqlpackage-win7-x64-en-US-15.0.4897.1\sqlpackage.exe /Action:Import /tcs:$sqlConnectionString /sf:C:\install\contosoclinic.bacpac
+sqlpackage /Action:Import /tcs:$sqlConnectionString /sf:C:\install\contosoclinic.bacpac
+
+# Configure additional AAD access to SQL
+# $connectionValues = $sqlConnectionString -replace ';',"`r`n" | ConvertFrom-StringData
+# $sqlLoginIdentity = "86e4fae4-b981-478e-91dc-6a9cc75a4dd9"
+# sqlcmd -S $connectionValues['Server'] -U $connectionValues['User ID'] -P $connectionValues['Password'] -Q "CREATE USER [$sqlLoginIdentity] FROM EXTERNAL PROVIDER;
+#     ALTER ROLE db_datareader ADD MEMBER [$sqlLoginIdentity];
+#     ALTER ROLE db_datawriter ADD MEMBER [$sqlLoginIdentity];
+#     ALTER ROLE db_ddladmin ADD MEMBER [$sqlLoginIdentity];
+#     GO"
 
 # Restart IIS
 net stop was /y
 net start w3svc
+
+
+
